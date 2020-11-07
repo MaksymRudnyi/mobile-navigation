2ecef9960039a4a2b4f4c096e4144bf10ba7f9ff
Maksym Rudnyi <maksym.rudnyi@fiverr.com>
2020-11-07 18:56:19 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelector('.target-burger').addEventListener('click', () => {
+        document.querySelector('body').classList.toggle('toggled')
+    })
+  })
